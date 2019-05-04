dc2101f30c9d3e61fd17e037c1db7aa1415e81e3
Pedro Thiago <pedrotrm@outlook.com.br>
2019-05-04 14:47:03 -0300

    Primeira Versao

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (S/N)");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'S')
+            {
+                Console.Write("Digite valor deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, nome, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, nome);
+            }
+            Console.WriteLine("\n Dados da conta: ");
+            Console.WriteLine(conta);
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados atualizados:");
+            conta.Deposito(quantia);
+            Console.WriteLine(conta);
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados atualizados:");
+            conta.Saque(quantia);
+            Console.WriteLine(conta);
+        }
+    }
+}
